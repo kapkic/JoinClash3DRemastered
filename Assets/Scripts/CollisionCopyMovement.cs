f53ffd6e7ea80e1842c90856ce04df125c24ad8b
ImperfectTortoise <ata.abg@gmail.com>
2021-05-02 01:33:11 +0300

    Enemy clash mechanic added

--- Assets/Scripts/CollisionCopyMovement.cs
+++ Assets/Scripts/CollisionCopyMovement.cs
@@ -8,34 +8,58 @@ public class CollisionCopyMovement : MonoBehaviour
 
     private Rigidbody rb;
 
-    private float movSpeed;
-    private bool collided;
-    private Vector3 movDir;
+    private GameObject enemyObj;
+    private bool collidedAlly;
+    private bool collidedEnemy;
+    private Vector3 enemyPos;
+    private Vector3 dummyPos;
+    private Vector3 enemyDir;
+    private Vector3 dummyDir;
+    private Vector3 afterCol = new Vector3(1,2,1);
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        collided = false;
+        collidedAlly = false;
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "Player" || collision.gameObject.name == "Dummy")
+        if (collision.gameObject.name == "Enemy")
         {
-            collided = true;
+            collidedEnemy = true;
+            collidedAlly = false;
+            enemyObj = collision.gameObject;
         }
+
+        if (collidedEnemy == false && collision.gameObject.name == "Player" || collision.gameObject.name == "Dummy")
+        {
+            collidedAlly = true;
+        }
+
     }
 
     private void FixedUpdate()
     {
-        if(collided == true)
+        if (collidedEnemy == true)
         {
-            //movSpeed = playerObj.GetComponent<Rigidbody>().velocity.magnitude;
-            //movDir = playerObj.GetComponent<Rigidbody>().velocity.normalized;
-            //playerObj.GetComponent<Rigidbody>().
+            enemyObj.GetComponent<BoxCollider>().size = afterCol;
+            dummyPos = rb.position;
+            enemyPos = enemyObj.GetComponent<Rigidbody>().position;
+            dummyDir = enemyPos - dummyPos;
+            dummyDir.Normalize();
+            enemyDir = dummyPos - enemyPos;
+            enemyDir.Normalize();
+            rb.velocity = dummyDir * 2;
+            enemyObj.GetComponent<Rigidbody>().velocity = enemyDir * 2;
+        }
+
+        if (collidedAlly == true)
+        {
             rb.velocity = playerObj.GetComponent<Rigidbody>().velocity;
         }
+
     }
 }
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject playerObj;
+
+    private Rigidbody rb;
+
+    private float movSpeed;
+    private bool collided;
+    private Vector3 movDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collided = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player" || collision.gameObject.name == "Dummy")
+        {
+            collided = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (collided == true)
+        {
+            //movSpeed = playerObj.GetComponent<Rigidbody>().velocity.magnitude;
+            //movDir = playerObj.GetComponent<Rigidbody>().velocity.normalized;
+            //playerObj.GetComponent<Rigidbody>().
+            rb.velocity = playerObj.GetComponent<Rigidbody>().velocity;
+        }
+    }
+}
